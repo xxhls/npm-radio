92ca6ca2128f713c7285eb8330b7d3adbd94e93d
xxhls <heyq020814@qq.com>
2024-03-19 11:49:10 +0800

    :sparkles: add logger

---
+++ src/log/index.ts
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+export const info = (message: string) => {
+    console.log(chalk.white.bgGray.bold(" INFO  \t"), chalk.gray(message));
+}
+
+export const debug = (message: string) => {
+    console.log(chalk.white.bgBlue.bold(" DEBUG \t"), chalk.blue(message));
+}
+
+export const warn = (message: string) => {
+    console.log(chalk.white.bgYellow.bold(" WARN  \t"), chalk.yellow(message));
+}
+
+export const error = (message: string) => {
+    console.log(chalk.white.bgRed.bold(" ERROR \t"), chalk.red(message));
+}
